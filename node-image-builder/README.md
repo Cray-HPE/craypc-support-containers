0678eba2631adf61451575698a06824ff6cc010f
Patrick Force <pforce@cray.com>
2020-02-18 16:52:39 -0700

    wip for updated qemu and google cloud image building support in the node-image-builder

---
+++ node-image-builder/README.md
@@ -0,0 +1,8 @@
+# VM/Node Image Builder
+
+Most of what is here is probably throw-away, but gets us what we need now ahead of things like:
+
+* Google Cloud interconnect
+* Some dedicated hardware for running QEMU/KVM builds, so something that supports building QCOW images
+
+The way this image serves its purpose is a bit inefficient right now considering, but we'll switch it around when we can
---
+++ node-image-builder/scripts/build-on-gcp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+project_id="vshasta-cray"
+source_disk_image_name="node-image-builder"
+instance_name="node-image-builder-$(date +%s)"
+zone="us-central1-a"
+subnet="default-network-us-central1"
+
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+  echo "Error: GOOGLE_APPLICATION_CREDENTIALS is not set"
+  exit 1
+fi
+gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+
+function cleanup() {
+  exit_status=$?
+  echo "Cleaning up..."
+  # ( gcloud compute instances delete $instance_name --quiet --zone $zone --project $project_id )
+  if [ -d /tmp/packer_cache ]; then
+    mv /tmp/packer_cache $(pwd)/
+  fi
+  exit $exit_status
+}
+
+trap cleanup EXIT
+
+echo "Creating builder GCP instance..."
+gcloud compute instances create $instance_name \
+  --project $project_id \
+  --zone $zone \
+  --machine-type=n1-standard-8 \
+  --boot-disk-size=2048GB \
+  --boot-disk-type=pd-ssd \
+  --image $source_disk_image_name \
+  --min-cpu-platform='Intel Haswell' \
+  --scopes='https://www.googleapis.com/auth/cloud-platform' \
+  --subnet $subnet
+
+echo "Setting up the GCP builder instance"
+gcloud compute scp $this_dir/setup-gcp-instance.sh builder@$instance_name:/tmp/ --zone $zone --project $project_id
+gcloud compute ssh --zone $zone --project $project_id builder@$instance_name -- 'sudo /tmp/setup-gcp-instance.sh '"$PACKER_VERSION"''
+
+echo "Syncing current directory source to the GCP instance and running: $@"
+rm -rf ./.build
+if [ -d $(pwd)/packer_cache ]; then
+  mv $(pwd)/packer_cache /tmp/
+fi
+gcloud compute scp $(pwd) builder@$instance_name:/tmp/build-src --recurse --zone $zone --project $project_id
+gcloud compute scp $GOOGLE_APPLICATION_CREDENTIALS builder@$instance_name:/tmp/build-src/key.json --zone $zone --project $project_id
+cat > /tmp/build.sh <<EOF
+#!/bin/bash
+sudo su -
+export GOOGLE_APPLICATION_CREDENTIALS=/tmp/build-src/key.json
+cd /tmp/build-src
+$@
+EOF
+gcloud compute scp /tmp/build.sh builder@$instance_name:/tmp/build.sh --recurse --zone $zone --project $project_id
+gcloud compute ssh --zone $zone --project $project_id builder@$instance_name -- 'chmod +x /tmp/build.sh'
+gcloud compute ssh --zone $zone --project $project_id builder@$instance_name -- '/tmp/build.sh'
+if [ -d /tmp/build-src/.build ]; then
+  gcloud compute scp builder@$instance_name:/tmp/build-src/.build $(pwd)/ --recurse --zone $zone --project $project_id
+fi
+
+exit 0
