0f0e194d1142e5e17f63242f5f1f6a132c17ea2c
Patrick Force <patrick.force@hpe.com>
2020-04-06 16:51:18 -0600

    make project id and subnet parameterized for node-image-builder instance

--- node-image-builder/scripts/build-on-gcp.sh
+++ node-image-builder/scripts/build-on-gcp.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-project_id="vshasta-cray"
+if [ -z "$PROJECT_ID" ]; then
+  PROJECT_ID="vshasta-cray"
+fi
+if [ -z "$PROJECT_SUBNET" ]; then
+  PROJECT_SUBNET="default-network-us-central1"
+fi
 source_disk_image_name="node-image-builder"
 instance_name="node-image-builder-$(date +%s)"
 zone="us-central1-a"
-subnet="default-network-us-central1"
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -19,7 +23,7 @@ gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
 function cleanup() {
   exit_status=$?
   echo "Cleaning up..."
-  gcloud compute instances delete $instance_name --quiet --zone $zone --project $project_id || true
+  gcloud compute instances delete $instance_name --quiet --zone $zone --project $PROJECT_ID || true
   if [ -d /tmp/packer_cache ]; then
     mv /tmp/packer_cache $(pwd)/
   fi
@@ -39,7 +43,7 @@ EOF
 
 echo "Creating builder GCP instance..."
 gcloud compute instances create $instance_name \
-  --project $project_id \
+  --project $PROJECT_ID \
   --zone $zone \
   --machine-type=n1-standard-8 \
   --boot-disk-size=2048GB \
@@ -47,31 +51,31 @@ gcloud compute instances create $instance_name \
   --image $source_disk_image_name \
   --min-cpu-platform='Intel Haswell' \
   --scopes='https://www.googleapis.com/auth/cloud-platform' \
-  --subnet $subnet
+  --subnet $PROJECT_SUBNET
 
 echo "Setting up the GCP builder instance"
-echo "Y" | gcloud compute scp $this_dir/setup-gcp-instance.sh builder@$instance_name:/tmp/ --zone $zone --project $project_id
-gcloud compute ssh --zone $zone --project $project_id builder@$instance_name -- '/tmp/setup-gcp-instance.sh '"$PACKER_VERSION"''
+echo "Y" | gcloud compute scp $this_dir/setup-gcp-instance.sh builder@$instance_name:/tmp/ --zone $zone --project $PROJECT_ID
+gcloud compute ssh --zone $zone --project $PROJECT_ID builder@$instance_name -- '/tmp/setup-gcp-instance.sh '"$PACKER_VERSION"''
 
 echo "Syncing current directory source to the GCP instance"
 rm -rf ./.build
 if [ -d $(pwd)/packer_cache ]; then
   mv $(pwd)/packer_cache /tmp/
 fi
-gcloud compute scp $(pwd) builder@$instance_name:/tmp/build-src --recurse --zone $zone --project $project_id
-gcloud compute scp $GOOGLE_APPLICATION_CREDENTIALS builder@$instance_name:/tmp/key.json --zone $zone --project $project_id
+gcloud compute scp $(pwd) builder@$instance_name:/tmp/build-src --recurse --zone $zone --project $PROJECT_ID
+gcloud compute scp $GOOGLE_APPLICATION_CREDENTIALS builder@$instance_name:/tmp/key.json --zone $zone --project $PROJECT_ID
 cat > /tmp/build.sh <<EOF
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=/tmp/key.json
-gcloud auth  activate-service-account --key-file /tmp/key.json
+gcloud auth activate-service-account --key-file /tmp/key.json
 cd /tmp/build-src
 $@
 EOF
-gcloud compute scp /tmp/build.sh builder@$instance_name:/tmp/build.sh --recurse --zone $zone --project $project_id
-gcloud compute ssh --zone $zone --project $project_id builder@$instance_name -- 'mkdir -p /tmp/build-src/http && ln -s /mnt/shasta-cd-repo /tmp/build-src/http/shasta-cd-repo'
+gcloud compute scp /tmp/build.sh builder@$instance_name:/tmp/build.sh --recurse --zone $zone --project $PROJECT_ID
+gcloud compute ssh --zone $zone --project $PROJECT_ID builder@$instance_name -- 'mkdir -p /tmp/build-src/http && ln -s /mnt/shasta-cd-repo /tmp/build-src/http/shasta-cd-repo'
 echo "Running operation"
-gcloud compute ssh --ssh-flag="-ServerAliveInterval=30" --zone $zone --project $project_id builder@$instance_name -- 'chmod +x /tmp/build.sh && /tmp/build.sh'
+gcloud compute ssh --ssh-flag="-ServerAliveInterval=30" --zone $zone --project $PROJECT_ID builder@$instance_name -- 'chmod +x /tmp/build.sh && /tmp/build.sh'
 echo "Syncing .build contents back"
-gcloud compute scp builder@$instance_name:/tmp/build-src/.build $(pwd)/ --recurse --zone $zone --project $project_id || true
+gcloud compute scp builder@$instance_name:/tmp/build-src/.build $(pwd)/ --recurse --zone $zone --project $PROJECT_ID || true
 
 exit 0
